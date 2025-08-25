4ce26b4ec0e9fbb612fe43d1727276127d3af279
Patrick Gunsolley <patrickgunsolley@gmail.com>
2025-08-25 05:03:39 -0700

    Integrated Vite

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import process from 'node:process';
+
+export default defineConfig({
+    build: {
+        emptyOutDir: false,
+        outDir: './webroot',
+        manifest: true,
+        rollupOptions: {
+            input: ['./webroot_src/json-editor.ts'],
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        strictPort: true,
+        origin: `${process.env.DDEV_PRIMARY_URL?.replace(/:\d+$/, "")}:5173`,
+    },
+});
